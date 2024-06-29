027e9cc9a3af8f71cbe44e4ce43b5d3e0fae0a13
MrFatra <ramadhanfatra2004@gmail.com>
2024-06-29 18:41:20 +0700

    feat(contact): put controller & logout

--- lib/screens/home.screen.dart
+++ lib/screens/home.screen.dart
@@ -1,5 +1,7 @@
+import 'package:chat_app/controller/contact.controller.dart';
 import 'package:chat_app/services/socket_io.client.service.dart';
 import 'package:chat_app/services/store.service.dart';
+import 'package:chat_app/widgets/alert.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
@@ -8,47 +10,65 @@ class HomeScreen extends StatelessWidget {
 
   final SocketController connectionController = Get.find<SocketController>();
   final AuthStorage _authStorage = Get.find<AuthStorage>();
+  final ContactController _contactController = Get.put<ContactController>(ContactController());
 
   final TextEditingController _messageController = TextEditingController();
+  final ScrollController _scrollController = ScrollController();
+  // final RxList<Contacts> contacts = [].obs;
+  final isLoading = false.obs;
+
+  void logout() async {
+    isLoading.value = true;
+    try {
+      await _authStorage.logout().then((value) => Get.offAllNamed('/login'));
+    } catch (err) {
+      debugPrint(err.toString());
+    } finally {
+      isLoading.value = false;
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: Text('Chat App'),
+        centerTitle: true,
+        actions: [
+          IconButton(
+            onPressed: () {
+              showAlert(
+                title: 'Warning',
+                message: 'Are you sure want to logout?',
+                confirmText: 'Logout',
+                buttonType: ButtonType.error,
+                lazy: isLoading,
+                onPressed: logout,
+              );
+            },
+            icon: Icon(Icons.logout),
+          ),
+        ],
+      ),
       body: SafeArea(
         child: Obx(() {
-          if (connectionController.socketConnected.value) {
+          if (connectionController.socketConnected.value && _contactController.data.value != '') {
             return Center(
-                child: Column(
-              children: [
-                const Text('You\'re Connected!'),
-                ElevatedButton(
-                    onPressed: () async {
-                     await _authStorage.logout().then((value) => Get.offAllNamed('/login'));
-                    },
-                    child: const Text('Logout')),
-                Expanded(
-                  child: ListView.builder(
-                    itemCount: connectionController.messages.length,
-                    itemBuilder: (context, index) {
-                      return Text(connectionController.messages[index]);
-                    },
-                  ),
-                ),
-                TextField(
-                  controller: _messageController,
-                  decoration: InputDecoration(
-                      hintText: 'Input message here...',
-                      suffixIcon: IconButton(
-                        icon: const Icon(Icons.send),
-                        onPressed: () {
-                          debugPrint(_messageController.text);
-                          connectionController
-                              .sendMessage(_messageController.text);
-                        },
-                      )),
-                ),
-              ],
-            ));
+              child: Column(
+                children: [
+                  Text(_contactController.data.value)
+                  // list of contact widget
+                  // Expanded(
+                    // child: ListView.builder(
+                    //   itemCount: connectionController.contacts.length,
+                    //   itemBuilder: (context, index) {
+                    //     return ContactWidget();
+                    //   },
+                    // ),
+                  // ),
+                ],
+              ),
+            );
           } else {
             return const Text('Not connected!');
           }
