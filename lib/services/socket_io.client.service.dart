c814c39c27fe3850dd5e62f4232e105a234f45fe
MrFatra <ramadhanfatra2004@gmail.com>
2024-07-02 08:00:38 +0700

    refactor: prefer global variable

--- lib/services/socket_io.client.service.dart
+++ lib/services/socket_io.client.service.dart
@@ -4,7 +4,7 @@ import 'package:get/get.dart';
 import 'package:socket_io_client/socket_io_client.dart' as IO;
 
 class SocketController extends GetxController {
-  late final IO.Socket _socket;
+  late final IO.Socket socket;
   var socketConnected = false.obs;
 
   @override
@@ -16,47 +16,47 @@ class SocketController extends GetxController {
   @override
   void onClose() {
     super.onClose();
-    _socket.dispose();
+    socket.dispose();
   }
 
   void connectToSocket() {
-    _socket = IO.io(
+    socket = IO.io(
         URL.SOCKET_HOST,
         IO.OptionBuilder()
             .setTransports(['websocket'])
             .enableReconnection()
             .build());
 
-    _socket.onConnect((_) {
+    socket.onConnect((_) {
       socketConnected.value = true;
       debugPrint('Connected to the server!');
-      debugPrint('Your socket id is ${_socket.id}');
+      debugPrint('Your socket id is ${socket.id}');
     });
 
-    _socket.onReconnect((data) {
+    socket.onReconnect((data) {
       debugPrint('Reconnected to the server!');
     });
 
-    _socket.onReconnectAttempt((data) {
+    socket.onReconnectAttempt((data) {
       debugPrint('Reconnecting to the server...');
     });
 
-    _socket.onReconnectError((data) {
+    socket.onReconnectError((data) {
       socketConnected.value = false;
       debugPrint('Error reconnecting to the server: $data');
     });
 
-    _socket.onReconnectFailed((data) {
+    socket.onReconnectFailed((data) {
       socketConnected.value = false;
       debugPrint('Failed to reconnect to the server: $data');
     });
 
-    _socket.onConnectError((data) {
+    socket.onConnectError((data) {
       socketConnected.value = false;
       debugPrint('Connection error: $data');
     });
 
-    _socket.onDisconnect((data) {
+    socket.onDisconnect((data) {
       socketConnected.value = false;
       debugPrint('Disconnected: $data');
     });
